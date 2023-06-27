8cbdda801c042da7a3155a4052eca5dab1de899e
sato_mel <satousakuya.hal@gmail.com>
2023-06-27 11:55:07 +0900

    title&result pad対応

--- Assets/tanuma/result.cs
+++ Assets/tanuma/result.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
 using UnityEngine;
 
 public class result : MonoBehaviour
@@ -13,7 +14,26 @@ public class result : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Return))
+        // ゲームパッド用
+        var GamePad = Gamepad.current;
+        bool aButtonDown = false; // 左（左スティック）
+        bool bButtonDown = false; // 左（左スティック）
+
+
+        if (GamePad != null)
+        {
+            if (GamePad.aButton.isPressed) // aボタンを押した時
+            {
+                aButtonDown = true;
+            }
+            if (GamePad.bButton.isPressed) // bボタンを押した時
+            {
+                bButtonDown = true;
+            }
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Return) || aButtonDown || bButtonDown) 
         {
             FadeManager.Instance.LoadScene("title", 1.0f);
             //SceneManager.LoadScene("stage1");    
--- Assets/tanuma/title.cs
+++ Assets/tanuma/title.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,14 +9,32 @@ public class title : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        // ゲームパッド用
+        var GamePad = Gamepad.current;
+        bool aButtonDown = false; // 左（左スティック）
+        bool bButtonDown = false; // 左（左スティック）
 
-        if (Input.GetKeyDown(KeyCode.Return))
+
+        if (GamePad != null)
+        {
+            if (GamePad.aButton.isPressed) // aボタンを押した時
+            {
+                aButtonDown = true;
+            }
+            if (GamePad.bButton.isPressed) // bボタンを押した時
+            {
+                bButtonDown = true;
+            }
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Return) || aButtonDown || bButtonDown)
         {
             FadeManager.Instance.LoadScene("stage2_test", 1.0f);
             //SceneManager.LoadScene("stage1");    
